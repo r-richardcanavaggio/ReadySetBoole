216f9425a23daecd41f64e289410f72c00ade91e
Robin Richard-Canavaggio <22174078+r-richardcanavaggio@users.noreply.github.com>
2025-10-26 18:02:31 +0100

    same with ex01

--- ex01/ex01.cpp
+++ ex01/ex01.cpp
@@ -6,12 +6,35 @@
 /*   By: rrichard <rrichard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/20 20:07:28 by rrichard          #+#    #+#             */
-/*   Updated: 2025/09/20 20:24:36 by rrichard         ###   ########.fr       */
+/*   Updated: 2025/10/26 18:00:52 by rrichard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include "multiplier.h"
+
+uint32_t	adder( uint32_t a, uint32_t b )
+{
+	int	x = a;
+	int	y = b;
+	int	carry;
+	
+	while (y != 0)
+	{
+		carry = (x & y) << 1;
+		x = x ^ y;
+		y = carry;
+	}
+	return (x);
+}
+
+uint32_t	multiplier( uint32_t a, uint32_t b )
+{
+	uint32_t sum = 0;
+
+	for (uint32_t i = 0; i < b; i++)
+		sum = adder(sum, a);
+	return (sum);
+}
 
 int	main( int argc, char **argv )
 {
@@ -22,8 +45,11 @@ int	main( int argc, char **argv )
 	}
 	try
 	{
-		int a = std::stoi(argv[1]);
-		int b = std::stoi(argv[2]);
+		long long a = std::stoll(argv[1]);
+		long long b = std::stoll(argv[2]);
+		if (a < 0 || b < 0)
+			return (std::cerr << "Arguments must be natural integers." << std::endl, 1);
+		a = static_cast<uint32_t>(a); b = static_cast<uint32_t>(b);
 		std::cout << multiplier(a, b) << std::endl;
 	}
 	catch (const std::exception &e)
