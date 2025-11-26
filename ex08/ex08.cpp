ae5358781714467eb39c531fc89f519e48781c55
Robin Richard-Canavaggio <22174078+r-richardcanavaggio@users.noreply.github.com>
2025-11-26 16:23:10 +0100

    forbidden library deleted

--- ex08/ex08.cpp
+++ ex08/ex08.cpp
@@ -6,27 +6,24 @@
 /*   By: rrichard <rrichard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 18:15:49 by rrichard          #+#    #+#             */
-/*   Updated: 2025/10/24 22:08:27 by rrichard         ###   ########.fr       */
+/*   Updated: 2025/11/26 15:37:26 by rrichard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <vector>
 #include <iostream>
 #include <string>
-#include <cmath>
 #include <sstream>
 
-using namespace std;
-
-vector<vector<int>>	powerset( vector<int> set )
+std::vector<std::vector<int32_t>>	powerset( std::vector<int32_t> set )
 {
-	vector<vector<int>>	res;
-	int				n = static_cast<int>(set.size());
+	std::vector<std::vector<int32_t>>	res;
+	size_t								n = set.size();
 
-	for (int mask = 0; mask < (1 << n); mask++)
+	for (size_t mask = 0; mask < (1 << n); mask++)
 	{
-		vector<int>	newSet;
-		for (int j = 0; j < n; j++)
+		std::vector<int32_t>	newSet;
+		for (size_t j = 0; j < n; j++)
 		{
 			if (mask & (1 << j))
 				newSet.push_back(set[j]);
@@ -39,20 +36,19 @@ vector<vector<int>>	powerset( vector<int> set )
 int main( int argc, char *argv[] )
 {
 	if (argc != 2)
-		return (cerr << "Wrong number of arguments" << endl, 1);
-	string			input = argv[1];
-	istringstream	iss(input);
-	vector<int>		v;
-	int				i;
+		return (std::cerr << "Wrong number of arguments" << std::endl, 1);
+	std::istringstream	iss(argv[1]);
+	std::vector<int>	v;
+	int32_t				i;
 	
 	while (iss >> i)
 		v.push_back(i);
-	vector<vector<int>> pwr = powerset(v);
+	std::vector<std::vector<int32_t>> pwr = powerset(v);
 	for (auto& i : pwr)
 	{
 		for (auto& j : i)
-			cout << j << " ";
-		cout << endl;
+			std::cout << j << " ";
+		std::cout << std::endl;
 	}
 	return (0);
 }
